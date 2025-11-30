18492fdd3c01e47bcbedd12b1eccab5ec4b4f1f1
oberien <jaro.fietz@gmx.de>
2025-11-30 23:29:36 +0100

    Add more gdb tests

--- src/tests.rs
+++ src/tests.rs
@@ -26,7 +26,8 @@ fn run_gdb(code: &str, expected: &str) {
         .output()
         .unwrap();
     let stdout = String::from_utf8_lossy(&output.stdout);
-    assert!(stdout.contains(expected), "stdout doesn't contain {expected:?}:\ncode:\n {code}\n\nstdout:\n{stdout}");
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    assert!(stdout.contains(expected) || stderr.contains(expected), "stdout/stderr don't contain {expected:?}:\ncode:\n {code}\n\nstdout:\n{stdout}\n-----\n\nstderr:\n{stderr}\n-----");
 }
 
 #[test]
@@ -226,6 +227,177 @@ fn test_gdb_convert() {
                 const_type $f64, #u64 {value:#x}
                 convert {typ}
             }}
-        "#), &format!("{gdb_actual:#x}"));
+        "#), &format!("{gdb_actual:#x}\n"));
+    }
+}
+
+#[test]
+fn test_gdb_abs() {
+    let tests = [
+        // "positive" generic type
+        ("constu 0x1337\nabs", "0x1337\n"),
+        // "negative" generic type
+        ("constu 0xffff_ffff_ffff_ffff\nabs", "0x1\n"),
+    ];
+    for (code, result) in tests {
+        run_gdb(&format!(r#"
+            #type $Foo {{
+                a: $u8,
+                b: $u8,
+                c: $u8,
+            }}
+            #var test {{
+                {code}
+            }}"#), result);
+    }
+}
+#[test]
+fn test_gdb_neg() {
+    let tests = [
+        // generic type
+        ("constu 0x1337\nneg", "0xffffffffffffecc9\n"),
+        // unsigned "positive" types that fit into the target type
+        ("const_type $u8, #u8 0x1\nneg", "0xff\n"),
+        ("const_type $u16, #u16 0x1\nneg", "0xffff\n"),
+        ("const_type $u32, #u32 0x1\nneg", "0xffffffff\n"),
+        ("const_type $u64, #u64 0x1\nneg", "0xffffffffffffffff\n"),
+        ("const_type $Foo, #u8 0x01, 0x00, 0x00\nneg", "0xffffff\n"),
+        // unsigned "negative" types that fit into the target type
+        ("const_type $u8, #u8 0x81\nneg", "0x7f\n"),
+        ("const_type $u16, #u16 0x8001\nneg", "0x7fff\n"),
+        ("const_type $u32, #u32 0x80000001\nneg", "0x7fffffff\n"),
+        ("const_type $u64, #u64 0x8000_0000_0000_0001\nneg", "0x7fffffffffffffff\n"),
+        ("const_type $Foo, #u8 0x01, 0x00, 0x80\nneg", "0x7fffff\n"),
+        // unsigned types that _don't_ fit into the target type
+        // -> undefined according to DWARF spec
+        ("const_type $u8, #u8 0x80\nneg", "0x80\n"),
+        ("const_type $u16, #u16 0x8000\nneg", "0x8000\n"),
+        ("const_type $u32, #u32 0x80000000\nneg", "0x80000000\n"),
+        ("const_type $u64, #u64 0x8000_0000_0000_0000\nneg", "0x8000000000000000\n"),
+        ("const_type $Foo, #u8 0x00, 0x00, 0x80\nneg", "0x800000\n"),
+        // check that the type stays the original type after neg
+        ("const_type $u8, #u8 0x1\nneg\nconst_type $u8, #u8 2\nplus", "0x1\n"),
+        ("const_type $u8, #u8 0x1\nneg\nconst_type $i8, #i8 2\nplus", "Incompatible types on DWARF stack\n"),
+        ("const_type $i8, #i8 0x1\nneg\nconst_type $u8, #i8 2\nplus", "Incompatible types on DWARF stack\n"),
+        ("const_type $i8, #i8 0x1\nneg\nconst_type $i8, #u8 2\nplus", "0x1\n"),
+    ];
+    for (code, result) in tests {
+        run_gdb(&format!(r#"
+            #type $Foo {{
+                a: $u8,
+                b: $u8,
+                c: $u8,
+            }}
+            #var test {{
+                {code}
+            }}"#), result);
+    }
+}
+
+#[test]
+fn test_gdb_plus() {
+    let tests = [
+        // "unsigned" generic type
+        ("constu 0x1\nconstu 0x1\nplus", "0x2\n"),
+        // "signed" generic type
+        ("consts 0x1\nconsts 0x1\nplus", "0x2\n"),
+        // mix signed and unsigned generic type
+        ("constu 0x1\nconsts 0x1\nplus", "0x2\n"),
+        // mix generic and typed integers
+        ("constu 0x1\nconst_type $u8, #u8 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        ("constu 0x1\nconst_type $u16, #u16 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        ("constu 0x1\nconst_type $u32, #u32 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        ("constu 0x1\nconst_type $u64, #u64 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        // mix signed and unsigned of same size
+        ("const_type $u8, #u8 0x1\nconst_type $i8, #i8 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        ("const_type $u16, #u16 0x1\nconst_type $i16, #i16 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        ("const_type $u32, #u32 0x1\nconst_type $i32, #i32 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        ("const_type $u64, #u64 0x1\nconst_type $i64, #i64 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        // mix unsigned of different sizes
+        ("const_type $u8, #u8 0x1\nconst_type $u16, #u16 0x1\nplus", "Incompatible types on DWARF stack\n"),
+        // mix signed of different sizes
+        ("const_type $i8, #i8 0x1\nconst_type $i16, #i16 0x1\nplus", "Incompatible types on DWARF stack\n"),
+    ];
+    for (code, result) in tests {
+        run_gdb(&format!(r#"
+            #type $Foo {{
+                a: $u8,
+                b: $u8,
+                c: $u8,
+            }}
+            #var test {{
+                {code}
+            }}"#), result);
+    }
+}
+
+#[test]
+fn test_gdb_not() {
+    let tests = [
+        ("const_type $f64, #u64 0x3ff0000000000000\nnot", "integral type expected in DWARF expression\n"),
+        ("const_type $u8, #u8 0b0000_1111\nnot", "0xf0\n"),
+        ("const_type $Foo, #u8 0x0f, 0xf0, 0x00\nnot", "0xff0ff0\n"),
+    ];
+    for (code, result) in tests {
+        run_gdb(&format!(r#"
+            #type $Foo {{
+                a: $u8,
+                b: $u8,
+                c: $u8,
+            }}
+            #var test {{
+                {code}
+            }}"#), result);
+    }
+}
+
+#[test]
+fn test_gdb_shr() {
+    let tests = [
+        ("const_type $f64, #u64 0x3ff0000000000000\nconstu 2\nshr", "Incompatible types on DWARF stack\n"),
+        ("const_type $u8, #u8 0x10\nconstu 2\nshr", "Incompatible types on DWARF stack\n"),
+        ("const_type $u8, #u8 0x10\nconst_type $u8, #u8 2\nshr", "0x4\n"),
+        ("const_type $i8, #i8 0x10\nconst_type $i8, #i8 2\nshr", "0x4\n"),
+        ("const_type $i8, #i8 -0x10\nconst_type $i8, #i8 -2\nshr", "0x0\n"),
+        ("const_type $i8, #i8 -0x10\nconst_type $i8, #i8 2\nshr", "0x3c\n"),
+        ("const_type $Foo, #u8 0x00, 0x00, 0x10\nconst_type $Foo, #u8 10, 0, 0\nshr", "0x400\n"),
+    ];
+    for (code, result) in tests {
+        run_gdb(&format!(r#"
+            #type $Foo {{
+                a: $u8,
+                b: $u8,
+                c: $u8,
+            }}
+            #var test {{
+                {code}
+            }}"#), result);
+    }
+}
+
+#[test]
+fn test_gdb_shra() {
+    let tests = [
+        ("const_type $f64, #u64 0x3ff0000000000000\nconstu 2\nshra", "Incompatible types on DWARF stack\n"),
+        ("const_type $u8, #u8 0x10\nconstu 2\nshra", "Incompatible types on DWARF stack\n"),
+        ("const_type $u8, #u8 0x10\nconst_type $u8, #u8 2\nshra", "0x4\n"),
+        ("const_type $u8, #u8 0xf0\nconst_type $u8, #u8 2\nshra", "0xfc\n"),
+        ("const_type $i8, #i8 0x10\nconst_type $i8, #i8 2\nshra", "0x4\n"),
+        ("const_type $i8, #i8 -0x10\nconst_type $i8, #i8 -2\nshra", "0xff\n"),
+        ("const_type $i8, #i8 -0x10\nconst_type $i8, #i8 2\nshra", "0xfc\n"),
+        ("const_type $i8, #i8 0x10\nconst_type $i8, #i8 100\nshra", "0x0\n"),
+        ("const_type $i8, #i8 -0x10\nconst_type $i8, #i8 100\nshra", "0xff\n"),
+        ("const_type $Foo, #u8 0x00, 0x00, 0x10\nconst_type $Foo, #u8 10, 0, 0\nshra", "no signed variant found for type, while evaluating DWARF expression\n"),
+    ];
+    for (code, result) in tests {
+        run_gdb(&format!(r#"
+            #type $Foo {{
+                a: $u8,
+                b: $u8,
+                c: $u8,
+            }}
+            #var test {{
+                {code}
+            }}"#), result);
     }
 }
